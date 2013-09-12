c6167537402fef7c2c65e5124c59f89a5b883d7a
Edward Mann <the@eddmann.com>
2013-09-12 12:45:24 +0100

    Added MergeSort Java folder, added alt bottom-up impl.

---
+++ java/sorts/MergeSort/MergeSortBottomUpAlt.java
@@ -0,0 +1,69 @@
+public class MergeSortBottomUpAlt {
+
+    private static void print(String lbl, int[] N)
+    {
+        System.out.print(lbl + ": ");
+        for (int n : N) System.out.print(n + " ");
+        System.out.print("\n");
+    }
+
+    private static void merge(int[] arr, int l, int m, int h)
+    {
+        int[] lf = new int[m - l + 1];
+        int[] rg = new int[h - m + 1];
+
+        for (int i = 0, k = l; k < m; i++, k++) {
+            lf[i] = arr[k]; // l..m-1
+        }
+
+        for (int i = 0, k = m; k < h; i++, k++) {
+            rg[i] = arr[k]; // m..h-1
+        }
+
+        lf[lf.length - 1] = Integer.MAX_VALUE;
+        rg[rg.length - 1] = Integer.MAX_VALUE;
+
+        for (int i = l, j = 0, k = 0; i < h; i++) {
+            if (lf[j] <= rg[k]) {
+                arr[i] = lf[j++];
+            } else {
+                arr[i] = rg[k++];
+            }
+        }
+    }
+
+    public static void sort(int[] arr)
+    {
+        if (arr.length <= 1) {
+            return;
+        }
+
+        int step = 1;
+        int l, m;
+
+        while (step < arr.length) {
+            l = 0;
+            m = step;
+
+            while (m + step <= arr.length) {
+                merge(arr, l, m, m + step);
+                l = m + step;
+                m = l + step;
+            }
+
+            if (m < arr.length) {
+                merge(arr, l, m, arr.length);
+            }
+
+            step *= 2;
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        int[] N = new int[] { 4, 3, 1, 2 };
+        sort(N);
+        print("Sorted", N);
+    }
+
+}
