498765d88901abf02d95c7e5140eb97a7da3e8e6
Edward Mann <the@eddmann.com>
2014-01-01 09:58:07 +0000

    Added Double and Single LL in C.

---
+++ c/doublylinkedlist.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+// clear && gcc doublylinkedlist.c -o doublylinkedlist && ./doublylinkedlist && rm ./doublylinkedlist
+
+struct node {
+    int item;
+    struct node *prev, *next;
+} *head, *tail;
+
+struct node* insert(int item, bool at_tail)
+{
+    struct node *ptr = (struct node*) malloc(sizeof(struct node));
+    ptr->item = item;
+    ptr->prev = ptr->next = NULL;
+
+    if (NULL == head) {
+        head = tail = ptr;
+    } else if (at_tail) {
+        tail->next = ptr;
+        ptr->prev = tail;
+        tail = ptr;
+    } else {
+        ptr->next = head;
+        head->prev = ptr;
+        head = ptr;
+    }
+
+    return ptr;
+}
+
+int delete(bool from_tail)
+{
+    if (NULL == head) {
+        return -1;
+    } else if (from_tail) {
+        struct node* ptr = tail;
+        int item = ptr->item;
+        tail = ptr->prev;
+        if (tail == NULL) head = tail;
+        else tail->next = NULL;
+        free(ptr);
+        ptr = NULL;
+        return item;
+    } else {
+        struct node* ptr = head;
+        int item = ptr->item;
+        head = ptr->next;
+        if (head == NULL) tail = head;
+        else head->prev = NULL;
+        free(ptr);
+        ptr = NULL;
+        return item;
+    }
+}
+
+void list()
+{
+    struct node *ptr = head;
+
+    while (NULL != ptr) {
+        printf("%d ", ptr->item);
+        ptr = ptr->next;
+    }
+
+    printf("\n");
+}
+
+int main()
+{
+    for (int i = 1; i <= 10; i++)
+        insert(i, i < 6);
+
+    list(); // 10 9 8 7 6 1 2 3 4 5
+
+    for (int i = 1; i <= 4; i++)
+        delete(i < 3);
+
+    list(); // 8 7 6 1 2 3
+}
---
+++ c/linkedlist.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+// clear && gcc linkedlist.c -o linkedlist && ./linkedlist && rm ./linkedlist
+
+struct node {
+    int item;
+    struct node *next;
+} *head, *tail;
+
+struct node* insert(int item, bool at_tail)
+{
+    struct node *ptr = (struct node*) malloc(sizeof(struct node));
+    ptr->item = item;
+    ptr->next = NULL;
+
+    if (NULL == head) {
+        head = tail = ptr;
+    } else if (at_tail) {
+        tail->next = ptr;
+        tail = ptr;
+    } else {
+        ptr->next = head;
+        head = ptr;
+    }
+
+    return ptr;
+}
+
+int delete(bool from_tail)
+{
+    if (NULL == head) {
+        return -1;
+    } else if (from_tail) {
+        if (head == tail) return delete(false);
+        struct node *ptr = head;
+        while (ptr->next != tail) ptr = ptr->next;
+        int item = ptr->next->item;
+        tail = ptr;
+        free(tail->next);
+        tail->next= NULL;
+        ptr = NULL;
+        return item;
+    } else {
+        struct node *ptr = head;
+        int item = ptr->item;
+        head = ptr->next;
+        if (head == NULL) tail = head;
+        free(ptr);
+        ptr = NULL;
+        return item;
+    }
+}
+
+void list()
+{
+    struct node *ptr = head;
+
+    while (NULL != ptr) {
+        printf("%d ", ptr->item);
+        ptr = ptr->next;
+    }
+
+    printf("\n");
+}
+
+int main()
+{
+    for (int i = 1; i <= 10; i++)
+        insert(i, i < 6);
+
+    list(); // 10 9 8 7 6 1 2 3 4 5
+
+    for (int i = 1; i <= 4; i++)
+        delete(i < 3);
+
+    list(); // 8 7 6 1 2 3
+}
