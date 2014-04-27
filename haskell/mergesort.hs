2361bed5b19c7c2744092e00e32b2db94d101c14
Edward Mann <the@eddmann.com>
2014-04-27 09:53:19 +0100

    Added pred MergeSort haskell version.

--- haskell/mergesort.hs
+++ haskell/mergesort.hs
@@ -4,12 +4,22 @@ merge ys [] = ys
 merge xs@(x:xt) ys@(y:yt) | x <= y = x : xt `merge` ys
                           | otherwise = y : xs `merge` yt
 
+merge' :: (a -> a -> Bool) -> [a] -> [a] -> [a]
+merge' _ xs [] = xs
+merge' _ [] ys = ys
+merge' p xs@(x:xt) ys@(y:yt) | p x y = x : merge' p xt ys
+                             | otherwise = y : merge' p xs yt
+
 split :: [a] -> ([a], [a])
 split xs = splitAt (length xs `quot` 2) xs
 
---split (x:y:zs) = let (xs, ys) = split zs in (x:xs, y:ys)
---split [x] = ([x], [])
---split [] = ([], [])
+-- split (x:y:zs) = let (xs, ys) = split zs in (x:xs, y:ys)
+-- split [x] = ([x], [])
+-- split [] = ([], [])
+
+-- split xs = go xs xs where
+--     go (x:xs) (_:_:zs) = (x:us,vs) where (us,vs) = go xs zs
+--     go xs _ = ([], xs)
 
 sort :: Ord a => [a] -> [a]
 sort [] = []
@@ -17,4 +27,13 @@ sort [x] = [x]
 sort xs = let (lft, rgt) = split xs
           in sort lft `merge` sort rgt
 
-main = print $ sort [ 4, 3, 2, 1 ]
+sort' :: (a -> a -> Bool) -> [a] -> [a]
+sort' _ [] = []
+sort' _ [x] = [x]
+sort' p xs = let (lft, rgt) = split xs
+             in merge' p (sort' p lft) (sort' p rgt)
+
+main = do
+    print $ sort ls
+    print $ sort' (<=) ls
+    where ls = [ 4, 3, 2, 1 ]
